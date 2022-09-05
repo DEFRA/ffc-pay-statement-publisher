300bd63aff8a0fb597b927a75d1f09b1a3d5fe45
John Watson <johnwatson484@hotmail.com>
2022-09-05 12:28:47 +0100

    Rename variable

--- app/publishing/publish.js
+++ app/publishing/publish.js
@@ -2,9 +2,9 @@ const retry = require('../retry')
 const { getFile } = require('../storage')
 const publishByEmail = require('./publish-by-email')
 
-const publish = async (request, filename, personalisation) => {
+const publish = async (email, filename, personalisation) => {
   const file = await retry(() => getFile(filename))
-  return publishByEmail(request, file, personalisation)
+  return publishByEmail(email, file, personalisation)
 }
 
 module.exports = publish
