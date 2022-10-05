e3b6868b5718e896cc523671ffa7c4ba31b740f4
John Watson <johnwatson484@hotmail.com>
2022-10-05 13:13:58 +0100

    Add retry test

---
+++ test/unit/retry.test.js
@@ -0,0 +1,40 @@
+const retry = require('../../app/retry')
+let mockFunction
+
+describe('retry', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockFunction = jest.fn().mockResolvedValue('success')
+  })
+
+  test('calls function', async () => {
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalled()
+  })
+
+  test('calls function once if successful', async () => {
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalledTimes(1)
+  })
+
+  test('retries function call once if fails', async () => {
+    mockFunction.mockRejectedValueOnce('error')
+    mockFunction.mockResolvedValueOnce('success')
+    await retry(mockFunction)
+    expect(mockFunction).toHaveBeenCalledTimes(2)
+  })
+
+  test('retries function call up to maximum retries', async () => {
+    mockFunction.mockRejectedValue('error')
+    try {
+      await retry(mockFunction, 1)
+    } catch {}
+    expect(mockFunction).toHaveBeenCalledTimes(2)
+  })
+
+  test('throws error if maximum retries exceeded', async () => {
+    mockFunction.mockRejectedValue('error')
+    const wrapper = async () => retry(mockFunction, 1)
+    expect(wrapper()).rejects.toThrow()
+  })
+})
