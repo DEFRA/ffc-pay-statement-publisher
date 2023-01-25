e59eb506930e53f3c3d03c1b58dcc7ce24b68d99
Marc Templeton <marcte@kainos.com>
2023-01-25 12:59:54 +0000

    added frn schema tests

--- app/schemas/components/frn.js
+++ app/schemas/components/frn.js
@@ -4,8 +4,10 @@ module.exports = {
   frn: Joi.number().integer().min(1000000000).max(9999999999).required()
     .messages({
       'number.base': 'The FRN must be a number.',
+      'number.integer': 'The FRN must be 10 digits.',
       'number.min': 'The FRN must be 10 digits.',
       'number.max': 'The FRN must be 10 digits.',
+      'any.required': 'FRN must be provided.',
       '*': 'The FRN must be a 10 digit number.'
     })
 }
