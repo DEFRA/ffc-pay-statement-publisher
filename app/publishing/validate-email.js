0573016b68deb87fc63895867702a93b4dd15340
Marc Templeton <marcte@kainos.com>
2023-02-08 14:41:37 +0000

    email validation

--- app/publishing/validate-email.js
+++ app/publishing/validate-email.js
@@ -1,9 +1,13 @@
 const schema = require('../schemas/components/email')
 
 const validateEmail = (email) => {
+  console.log('before:', email)
+
   const result = schema.validate(email, {
     abortEarly: false
   })
+
+  console.log('after:', result.error)
 
   if (result.error) {
     throw new Error(`Email is invalid: ${result.error.message}`)
