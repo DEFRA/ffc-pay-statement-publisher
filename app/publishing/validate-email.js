49910fbae590578a0958ff6642cf0c610e140145
Marc Templeton <marcte@kainos.com>
2023-02-13 11:17:05 +0000

    removing test logs

--- app/publishing/validate-email.js
+++ app/publishing/validate-email.js
@@ -1,14 +1,9 @@
 const schema = require('../schemas/components/email')
 
 const validateEmail = (email) => {
-  console.log('before: "', email, '"')
-  console.log('type: "', typeof (email), '"')
-
   const result = schema.validate(email, {
     abortEarly: false
   })
-
-  console.log('after:', result.error)
 
   if (result.error) {
     throw new Error(`Email is invalid: ${result.error.message}`)
