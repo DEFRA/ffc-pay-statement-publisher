a43f900835891b1b8fa6d6ddc52155df9ebcbf86
Marc Templeton <marcte@kainos.com>
2023-02-17 09:56:20 +0000

    using object interpretation

--- app/messaging/send-message.js
+++ app/messaging/send-message.js
@@ -1,10 +1,10 @@
-const ffcMessaging = require('ffc-messaging')
+const { MessageSender } = require('ffc-messaging')
 
 const createMessage = require('./create-message')
 
 const sendMessage = async (body, type, config) => {
   const message = createMessage(body, type)
-  const sender = new ffcMessaging.MessageSender(config)
+  const sender = new MessageSender(config)
   await sender.sendMessage(message)
   await sender.closeConnection()
 }
