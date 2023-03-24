56f9ba3c6e4918af9c46f30e9bae029d514675de
Marc Templeton <marcte@kainos.com>
2023-03-24 14:51:08 +0000

    missed

--- app/publishing/publish-by-email.js
+++ app/publishing/publish-by-email.js
@@ -6,7 +6,7 @@ const util = require('util')
 const publishByEmail = async (email, file, personalisation) => {
   const notifyClient = new NotifyClient(config.notifyApiKey)
   const a = notifyClient.prepareUpload(file, { confirmEmailBeforeDownload: true })
-  const b = notifyClient.sendEmail(config.notifyEmailTemplateKey, email, {
+  const b = await notifyClient.sendEmail(config.notifyEmailTemplateKey, email, {
     personalisation: {
       link_to_file: a,
       ...personalisation
