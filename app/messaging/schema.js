23fd45a9851f3fc97faa3366cf5835fdaf2295be
Marc Templeton <marcte@kainos.com>
2023-06-01 13:28:23 +0100

    tests passing

--- app/messaging/schema.js
+++ app/messaging/schema.js
@@ -6,6 +6,7 @@ module.exports = Joi.object({
   frn: Joi.number().integer().min(1000000000).max(9999999999).required(),
   sbi: Joi.number().integer().min(105000000).max(999999999).required(),
   email: Joi.string().optional().allow('', null),
+  documentReference: Joi.string().optional().allow('', null),
   address: Joi.object({
     line1: Joi.string().optional().allow('', null),
     line2: Joi.string().optional().allow('', null),
