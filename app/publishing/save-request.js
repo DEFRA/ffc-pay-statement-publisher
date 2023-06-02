6a9ba28b3b5bb1358ac3f0245392db7298435191
Marc Templeton <marcte@kainos.com>
2023-06-02 10:05:17 +0100

    moved func into own file

--- app/publishing/save-request.js
+++ app/publishing/save-request.js
@@ -1,5 +1,6 @@
 const db = require('../data')
 
+const saveStatement = require('./save-statement')
 const sendCrmMessage = require('../messaging/send-crm-message')
 const createFailure = require('../monitoring/create-failure')
 
@@ -19,23 +20,6 @@ const saveRequest = async (request, reference, method, reason) => {
     await transaction.rollback()
     throw err
   }
-}
-
-const saveStatement = async (request, timestamp, transaction) => {
-  return db.statement.create({
-    ...request,
-    addressLine1: request.address.line1,
-    addressLine2: request.address.line2,
-    addressLine3: request.address.line3,
-    addressLine4: request.address.line4,
-    addressLine5: request.address.line5,
-    postcode: request.address.postcode,
-    schemeName: request.scheme.name,
-    schemeShortName: request.scheme.shortName,
-    schemeYear: request.scheme.year,
-    schemeFrequency: request.scheme.frequency,
-    received: timestamp
-  }, { transaction })
 }
 
 const saveDelivery = async (statementId, method, reference, timestamp, transaction) => {
