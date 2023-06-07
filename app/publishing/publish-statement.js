2ae72ca7f98988ce6eaee641fd279db898f783e9
Marc Templeton <marcte@kainos.com>
2023-06-07 15:28:46 +0100

    adding dup check

--- app/publishing/publish-statement.js
+++ app/publishing/publish-statement.js
@@ -1,19 +1,26 @@
 const { EMAIL } = require('../constants/methods')
 
-const getPersonalisation = require('./get-personalisation')
-const handlePublishReasoning = require('./handle-publish-reasoning')
-const publish = require('./publish')
-const saveRequest = require('./save-request')
+const { getExistingDocument } = require('../processing/publish')
 const validateEmail = require('./validate-email')
+const getPersonalisation = require('./get-personalisation')
+const publish = require('./publish')
+const handlePublishReasoning = require('./handle-publish-reasoning')
+const saveRequest = require('./save-request')
 
 const publishStatement = async (request) => {
   let reason
   let response
+
   try {
-    validateEmail(request.email)
-    const personalisation = getPersonalisation(request.scheme.name, request.scheme.shortName, request.scheme.year, request.scheme.frequency, request.businessName)
-    response = await publish(request.email, request.filename, personalisation)
-    console.log(`Statement published: ${request.filename}`)
+    const existingDocument = await getExistingDocument(request.documentReference)
+    if (existingDocument) {
+      console.info(`Duplicate document received, skipping ${existingDocument.documentReference}`)
+    } else {
+      validateEmail(request.email)
+      const personalisation = getPersonalisation(request.scheme.name, request.scheme.shortName, request.scheme.year, request.scheme.frequency, request.businessName)
+      response = await publish(request.email, request.filename, personalisation)
+      console.log(`Statement published: ${request.filename}`)
+    }
   } catch (err) {
     reason = handlePublishReasoning(err)
   } finally {
