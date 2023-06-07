0ea145e2ac8ba2fd89b0a3b283e810064d20e365
Marc Templeton <marcte@kainos.com>
2023-06-07 15:58:07 +0100

    updating mock objects/comps

--- test/mocks/components/filename.js
+++ test/mocks/components/filename.js
@@ -1,9 +1,10 @@
-const _2022 = require('../components/marketing-year')
+const MARKETING_YEAR = require('../components/marketing-year')
 const FRN = require('./frn')
 const TIMESTAMP = require('./timestamp')
 
 const { SHORT_NAMES } = require('../../../app/constants/scheme-names')
 
 module.exports = {
-  SFI: `FFC_PaymentStatement_${SHORT_NAMES.SFI}_${_2022}_${FRN}_${TIMESTAMP}.pdf`
+  SCHEDULE: `FFC_PaymentSchedule_${SHORT_NAMES.SFI}_${MARKETING_YEAR}_${FRN}_${TIMESTAMP}.pdf`.replace(/\s/g, ''),
+  STATEMENT: `FFC_PaymentStatement_${SHORT_NAMES.SFI}_${MARKETING_YEAR}_${FRN}_${TIMESTAMP}.pdf`.replace(/\s/g, '')
 }
