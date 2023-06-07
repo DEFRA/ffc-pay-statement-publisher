fa30fbe09b2bd19a710f35fcdf5426390ed5d9bc
Marc Templeton <marcte@kainos.com>
2023-06-07 13:47:06 +0100

    tidy up

--- test/unit/schemas/components/document-reference.test.js
+++ test/unit/schemas/components/document-reference.test.js
@@ -4,89 +4,89 @@ const schema = require('../../../../app/schemas/components/document-reference')
 
 describe('document reference schema', () => {
   describe.each([
-    { name: 'DOCUMENT_REFERENCE', documentReference: DOCUMENT_REFERENCE, expected: DOCUMENT_REFERENCE },
-    { name: 'string DOCUMENT_REFERENCE', documentReference: String(DOCUMENT_REFERENCE), expected: DOCUMENT_REFERENCE },
-    { name: 'undefined', documentReference: undefined, expected: undefined }
-  ])('when document reference is $name', ({ name, documentReference, expected }) => {
+    { name: 'DOCUMENT_REFERENCE', value: DOCUMENT_REFERENCE, expected: DOCUMENT_REFERENCE },
+    { name: 'string DOCUMENT_REFERENCE', value: String(DOCUMENT_REFERENCE), expected: DOCUMENT_REFERENCE },
+    { name: 'undefined', value: undefined, expected: undefined }
+  ])('when document reference is $name', ({ name, value, expected }) => {
     test('returns an object', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result).toBeInstanceOf(Object)
     })
 
     test('returns an object with 1 key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toHaveLength(1)
     })
 
     test('returns an object with "value" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('value')
     })
 
     test(`returns ${expected} for key "value"`, () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.value).toBe(expected)
     })
   })
 
   describe.each([
-    { name: '0', documentReference: 0, expected: 0 },
-    { name: '"0"', documentReference: '0', expected: 0 },
-    { name: '-1', documentReference: -1, expected: -1 },
-    { name: '"-1"', documentReference: '-1', expected: -1 }
-  ])('when document reference is $name', ({ name, documentReference, expected }) => {
+    { name: '0', value: 0, expected: 0 },
+    { name: '"0"', value: '0', expected: 0 },
+    { name: '-1', value: -1, expected: -1 },
+    { name: '"-1"', value: '-1', expected: -1 }
+  ])('when document reference is $name', ({ name, value, expected }) => {
     test('returns an object', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result).toBeInstanceOf(Object)
     })
 
     test('returns an object with 2 keys', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toHaveLength(2)
     })
 
     test('returns an object with "value" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('value')
     })
 
     test(`returns ${expected} for key "value"`, () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.value).toBe(expected)
     })
 
     test('returns an object with "error" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('error')
     })
 
     test('returns an Error for key "error"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error).toBeInstanceOf(Error)
     })
 
     test('returns an object with 4 keys for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toHaveLength(4)
     })
 
     test('returns an object with "type" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('type')
     })
 
     test('returns "number.positive" for key "error.details[0].type"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error.details[0].type).toBe('number.positive')
     })
 
     test('returns an object with "message" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('message')
     })
 
     test('returns "The document reference must be a number greater than 0." for key "error.details[0].message"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       console.log(result)
       console.log(result.error)
       expect(result.error.details[0].message).toBe('The document reference must be a number greater than 0.')
@@ -94,123 +94,123 @@ describe('document reference schema', () => {
   })
 
   describe.each([
-    { name: '"a"', documentReference: 'a', expected: 'a' },
-    { name: 'null', documentReference: null, expected: null },
-    { name: 'false', documentReference: false, expected: false },
-    { name: 'true', documentReference: true, expected: true }
-  ])('when document reference is $name', ({ name, documentReference, expected }) => {
+    { name: '"a"', value: 'a', expected: 'a' },
+    { name: 'null', value: null, expected: null },
+    { name: 'false', value: false, expected: false },
+    { name: 'true', value: true, expected: true }
+  ])('when document reference is $name', ({ name, value, expected }) => {
     test('returns an object', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result).toBeInstanceOf(Object)
     })
 
     test('returns an object with 2 keys', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toHaveLength(2)
     })
 
     test('returns an object with "value" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('value')
     })
 
     test(`returns ${expected} for key "value"`, () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.value).toBe(expected)
     })
 
     test('returns an object with "error" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('error')
     })
 
     test('returns an Error for key "error"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error).toBeInstanceOf(Error)
     })
 
     test('returns an object with 4 keys for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toHaveLength(4)
     })
 
     test('returns an object with "type" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('type')
     })
 
     test('returns "number.base" for key "error.details[0].type"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error.details[0].type).toBe('number.base')
     })
 
     test('returns an object with "message" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('message')
     })
 
     test('returns "The document reference must be a number." for key "error.details[0].message"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error.details[0].message).toBe('The document reference must be a number.')
     })
   })
 
   describe.each([
-    { name: '[]', documentReference: [], expected: [] },
-    { name: '{}', documentReference: {}, expected: {} }
-  ])('when document reference is $name', ({ name, documentReference, expected }) => {
+    { name: '[]', value: [], expected: [] },
+    { name: '{}', value: {}, expected: {} }
+  ])('when document reference is $name', ({ name, value, expected }) => {
     test('returns an object', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result).toBeInstanceOf(Object)
     })
 
     test('returns an object with 2 keys', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toHaveLength(2)
     })
 
     test('returns an object with "value" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('value')
     })
 
     test(`returns ${expected} for key "value"`, () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.value).toStrictEqual(expected)
     })
 
     test('returns an object with "error" key', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result)).toContain('error')
     })
 
     test('returns an Error for key "error"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error).toBeInstanceOf(Error)
     })
 
     test('returns an object with 4 keys for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toHaveLength(4)
     })
 
     test('returns an object with "type" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('type')
     })
 
     test('returns "number.base" for key "error.details[0].type"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error.details[0].type).toBe('number.base')
     })
 
     test('returns an object with "message" key for key "error.details[0]"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(Object.keys(result.error.details[0])).toContain('message')
     })
 
     test('returns "The document reference must be a number." for key "error.details[0].message"', () => {
-      const result = schema.validate(documentReference)
+      const result = schema.validate(value)
       expect(result.error.details[0].message).toBe('The document reference must be a number.')
     })
   })
